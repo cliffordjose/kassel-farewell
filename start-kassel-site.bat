44443b098566cbdc36f392d1a31b6a3e743b3909
Clifford Jose <cliffordjose@users.noreply.github.com>
2026-09-21 04:17:17 +0530

    Fix Vercel 404 by adding vercel.json and renaming backend

--- start-kassel-site.bat
+++ start-kassel-site.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d "%~dp0"
-start "Kassel Wishes Server" /min node server.js
+start "Kassel Wishes Server" /min node local-server.js
 timeout /t 2 /nobreak >nul
 start "" http://localhost:3000
